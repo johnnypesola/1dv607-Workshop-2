289513732b6403597ad680eb6bb9ac9e7282a6da
Johnny Pesola <johnny@pesola.eu>
2015-10-01 23:18:47 +0200

    DAL extensions klass

---
+++ Workshop2/Extensions/DALExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop2.Extensions
+{
+    class DALExtensions
+    {
+        enum BoatType { Sailboat, Motorsailer, Kayak, Other };
+
+        // Custom Safe SqlDataReader methods, with null checks
+        public static BoatType GetSafeString(this SQLiteDataReader reader, int columnIndex)
+        {
+            string _returnValue = reader.GetString(columnIndex);
+            
+            switch (_returnValue)
+            {
+                case "Sailboat": return BoatType.Sailboat;
+                case "Motorsailer": return BoatType.Motorsailer;
+                case "Kayak": return BoatType.Kayak;
+                case "Other": return BoatType.Other;
+            }
+
+            return BoatType.Other;
+        }
+
+    }
+}
