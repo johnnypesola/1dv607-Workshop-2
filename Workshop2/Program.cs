6a682070eaeb5c02853f3ab0cdecb48c9d3e14a8
Johnny Pesola <johnny@pesola.eu>
2015-10-02 13:21:30 +0200

    Removed testcode

--- Workshop2/Program.cs
+++ Workshop2/Program.cs
@@ -14,40 +14,9 @@ namespace Workshop2
         {
 
             // Uncomment this if you want to reset all the data in database to defaults.
-            //Model.DAL.DALBase.SetupTables();
+            Model.DAL.DALBase.SetupTables();
 
-
-
-            MemberService memberService = new MemberService();
-
-
-            List<Member> members = memberService.GetAllMembers();
-
-            // Add boat to member
-            //memberService.SaveBoat(members[0], new Boat { BoatType = "Canoe", Length = 12, MemberId = members[0].Id });
-//            memberService.SaveBoat(members[0], new Boat { BoatType = "Motorsailer", Length = 12, MemberId = members[0].Id });
-            
-            // 
-            members = memberService.GetAllMembers();
-
-            foreach (Member member in members)
-            {
-                Console.WriteLine("{0}, {1}", member.Name, member.Boats.Count);
-            }
-
-            Boat boatToModify = members[0].Boats[0];
-
-            //boatToModify.Length = 999;
-
-            //memberService.SaveBoat(members[0], boatToModify);
-
-            memberService.DeleteBoat(members[0], boatToModify);
-
-            Console.ReadLine();
-
-
-
-            //BoatClubApp.generateMenu();
+            BoatClubApp.generateMenu();
         }
     }
 }
