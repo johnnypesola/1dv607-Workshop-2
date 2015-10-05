d6f359ac46591e49baa4aee14f744a4252bf9227
Matilda laptop <ingman.m@gmail.com>
2015-10-05 14:34:35 +0200

    Post merge

--- Workshop2/Program.cs
+++ Workshop2/Program.cs
@@ -10,14 +10,30 @@ namespace Workshop2
 {
     class Program
     {
+        private BoatClubApp _boatClubApp;
+
+        // Properties
+        private BoatClubApp BoatClubApp
+        {
+            // Auto create object if needed
+            get
+            {
+                return _boatClubApp ?? (_boatClubApp = new BoatClubApp());
+            }
+        }
         static void Main(string[] args)
         {
 
             // Uncomment this if you want to reset all the data in database to defaults.
             Model.DAL.DALBase.SetupTables();
-
-
-            BoatClubApp.generateMenu();
+            BoatClubApp bc = new BoatClubApp();
+            bc.generateMenu();
+            //MemberController memberController;
+            // Uncomment this if you want to reset all the data in database to defaults.
+            Model.DAL.DALBase.SetupTables();
+            // Create new member controller, it should do stuff with members
+            //memberController = new MemberController();
+            bc.generateMenu();
         }
     }
 }
